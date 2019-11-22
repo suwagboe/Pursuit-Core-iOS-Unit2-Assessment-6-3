200088ce8c055d88cc62fc59aa31d56128fbc8b2
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-22 12:05:47 -0500

    got the colors to FINALLY show after reading all of the instructions

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by World Domination a Brand on 11/22/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/mainViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/mainViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+//MARK: need to figure out how to make the colors populate in this view controller
+// should i make a custom cell and try doing it there?? 
 class mainViewController: UIViewController {
 
     // this is connected and it has constraints
@@ -59,6 +61,8 @@ extension mainViewController: UITableViewDataSource {
         // allows for the cell to be reused if it is there if not then a new cell will be created.
         let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
         
+        
+        
         // this variable gives me access to the propeties from colors which is of type crayon.
         let crayon = colors[indexPath.row]
         
@@ -68,8 +72,17 @@ extension mainViewController: UITableViewDataSource {
         // configure cell
         cell.textLabel?.text = crayon.name
         cell.detailTextLabel?.text = crayon.hex
-        cell.backgroundColor = UIColor(named: crayon.hex)
+        //cell.tintColor
+       // cell.backgroundView?.backgroundColor = UIColor(red: CGFloat(crayon.red), green: CGFloat(crayon.green), blue: CGFloat(crayon.blue), alpha: 1.0)
         
+        
+        // only the black color row shows....
+        // had to divide by 255
+        // also need to red instructions
+        cell.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1.0)
+
+
+      
         return cell
         
     }
@@ -77,3 +90,60 @@ extension mainViewController: UITableViewDataSource {
   
 }
 
+
+
+//https://www.hackingwithswift.com/example-code/uicolor/how-to-convert-a-hex-color-to-a-uicolor
+// the below extention allows for UIColors to access the hex value instead of just the string...
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
+
+
+/*
+        DONT KNOW IF I NEED YET
+ 
+ extension UITableViewCell {
+     var selectionColor: UIColor {
+         set {
+             let view = UIView()
+             view.backgroundColor = newValue
+             self.selectedBackgroundView = view
+         }
+         get {
+             return self.selectedBackgroundView?.backgroundColor ?? UIColor.clear
+         }
+     }
+ }
+ 
+ 
+ 
+ 
+ let backgroundView = UIView()
+       backgroundView.backgroundColor = UIColor.red
+       cell.selectedBackgroundView = backgroundView
+       
+ */
