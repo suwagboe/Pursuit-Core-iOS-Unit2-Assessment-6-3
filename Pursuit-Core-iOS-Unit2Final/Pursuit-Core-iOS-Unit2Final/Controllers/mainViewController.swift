876feaa95457a1cc58f2b83da4cbdf02e1d15fd2
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-22 10:52:44 -0500

    " the rows show but the data doesn't populate"

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/mainViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/mainViewController.swift
@@ -10,13 +10,54 @@ import UIKit
 
 class mainViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var colors = [Crayon]() {
+        didSet{
+            // loads the tableView
+            tableView.reloadData()
+        }
+    }
     
   override func viewDidLoad() {
     super.viewDidLoad()
    
-    
+    // this allows for the data to populate in the tableview when it first loads
+    tableView.dataSource = self
   }
 
 
+}
+
+extension mainViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // allows for the cell to be reused if it is there if not then a new cell will be created.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        // this variable gives me access to the propeties from colors which is of type crayon.
+        let crayon = colors[indexPath.row]
+        
+        // configure cell
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+        cell.backgroundColor = UIColor(named: crayon.hex)
+        
+        return cell
+        
+    }
+   
+    //Do I need this function in order to populate data
+    func numberOfSections(in tableView: UITableView) -> Int {
+           colors.count
+       }
+    
 }
 
