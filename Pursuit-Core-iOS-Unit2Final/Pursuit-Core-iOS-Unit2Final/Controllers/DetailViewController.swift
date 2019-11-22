8f1ea8dd289eb6f9cbc93a6983f36b81610376c2
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-22 14:20:58 -0500

    the sliders now work .. added tags to them so the values can be accessed.. I didn't need to divide again after the first time, that is what  made the screen black

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -19,26 +19,29 @@ class DetailViewController: UIViewController {
     // all sliders defaults will be set inside of the properties inspector to limit the amount of code typed.
     
     // variable for the red slider
-    @IBOutlet weak var redSliderLabel: UILabel!
+  
+   // use this label to change the color of the fonts based on color of background
+    @IBOutlet var sliderOutlets: [UILabel]!
+    
     @IBOutlet weak var redSlider: UISlider!
     
     // variables for the blue slider
-    @IBOutlet weak var blueSliderLabel: UILabel!
+  //  @IBOutlet weak var blueSliderLabel: UILabel!
     @IBOutlet weak var blueSlider: UISlider!
     
     // variables for the green slider
-    @IBOutlet weak var greenSliderLabel: UILabel!
+  //  @IBOutlet weak var greenSliderLabel: UILabel!
     @IBOutlet weak var greenSlider: UISlider!
     
     // variable for the alpha slider that is preset to be equal to 1.0
-    @IBOutlet weak var alphaStepperLabel: UILabel!
+  //  @IBOutlet weak var alphaStepperLabel: UILabel!
     @IBOutlet weak var alphaStepper: UIStepper!
     
     @IBOutlet weak var restButton: UIButton!
     
     // takes in selected crayon from previous controller
-    var selectedColor: Crayon?
-    
+    var selectedColor: Crayon!
+    // does not need a didSet it messes up my below guard statement
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,8 +51,8 @@ class DetailViewController: UIViewController {
     }
     
     func loadBackgroundColor() {
-        // unwrapp the imported color from the main ViewController
-        guard let detailsCrayon = selectedColor else {
+        // unwrapp the imported color from the main ViewController so that way it is no longer an optional
+         guard let detailsCrayon = selectedColor else {
             fatalError("couldn't set the selectedColor properly need to redue. ")
         }
         
@@ -62,22 +65,50 @@ class DetailViewController: UIViewController {
         
     }
     
+
+    
     //MARK: ACTIONS MAKE IMPACTS
     
     //action for all the sliders below
     
-    @IBAction func redSliderChangedO(_ sender: UISlider ){
-       // view.backgroundColor
+    @IBAction func SliderChangedO(_ sender: UISlider ){
         
-    }
+       
+        
+        var redVal = redSlider.value
+        var greenVal = greenSlider.value
+        var blueVal = blueSlider.value
+        if sender.tag == 0 {
+                redVal = sender.value
+            }
+        else if sender.tag == 1{
+            blueVal = sender.value
+            }
+        else if sender.tag == 2{
+                greenVal = sender.value;
+            }
+        view.backgroundColor = UIColor(displayP3Red: CGFloat(redVal), green: CGFloat(greenVal), blue: CGFloat(blueVal), alpha: 1.0)
     
-    @IBAction func blueSliderChangedO(_ sender: UISlider ){
-    }
+        }
+//        var selectedColorAmountOfRed: CGFloat = (CGFloat(selectedColor!.red/255)) {
+//               didSet {
+//                   print("amount of red is CHANGING")
+//               }
+//           }
+    //    selectedColorAmountOfRed = CGFloat(sender.value)
+//        view.backgroundColor = UIColor(displayP3Red: CGFloat(sender.value/255) , green: CGFloat(selectedColor!.green), blue: CGFloat(selectedColor!.blue), alpha: 1.0)
+        
+        
+//        guard let detailsCrayon = selectedColor else {
+//            fatalError("couldn't set the selectedColor properly need to redue. ")
+//        }
+//
+//        view.backgroundColor =  UIColor(displayP3Red: CGFloat(sender.value/255), green: CGFloat(detailsCrayon.green/255), blue: CGFloat(detailsCrayon.blue/255), alpha: 1.0)
     
-    @IBAction func greenSliderChangedO(_ sender: UISlider ){
-    }
     
-    @IBAction func alphaStepperChangedO(_ sender: UIStepper ){
+    
+    
+ @IBAction func alphaStepperChangedO(_ sender: UIStepper ){
     }
     
 }
